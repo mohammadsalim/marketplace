2bde440bbbcc571e5cd76cd171cb9294339d52a9
Mohammad Salim <jd2474ev@gmail.com>
2022-02-27 22:45:37 -0500

    Created a test to check succcess and failure for product must have a name, and must have a price

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -13,16 +13,28 @@ contract Marketplace {
     bool purchased;
   }
 
+  event ProductCreated(
+    uint id,
+    string name,
+    uint price,
+    address owner,
+    bool purchased
+  );
+
   constructor() public {
     name = "Salims Marketplace";
   }
 
   function createProduct(string memory _name, uint _price) public {
-    // Make sure parameters are correct
+    // Require a name
+    require(bytes(_name).length > 0);
+    // Require a valid price
+    require(_price > 0);
     // Increment product count
     productCount ++;
     // Create the product
     products[productCount] = Product(productCount, _name, _price, msg.sender, false);
     // Trigger an event
+    emit ProductCreated(productCount, _name, _price, msg.sender, false);
   }
 }
