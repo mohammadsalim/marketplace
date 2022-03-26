76b8f0a2b4df854953f509aa4af56f83526d48b0
Mohammad Salim <jd2474ev@gmail.com>
2022-03-26 13:27:25 -0400

    Created a purchaseProduct function and connected it to the buy button

--- src/components/App.js
+++ src/components/App.js
@@ -33,14 +33,21 @@ class App extends Component {
         // Load account
     const accounts = await web3.eth.getAccounts()
     console.log(accounts)
-    this.setState({ account: accounts[0]})
+    this.setState({ account: accounts[0] })
     const networkId = await web3.eth.net.getId()
     const networkData = Marketplace.networks[networkId]
     if(networkData) {
       const marketplace = web3.eth.Contract(Marketplace.abi, networkData.address)
       this.setState({ marketplace })
       const productCount = await marketplace.methods.productCount().call()
-      console.log(productCount.toString())
+      this.setState({ productCount })
+      // Load products
+      for (var i = 1; i <= productCount; i++) {
+        const product = await marketplace.methods.products(i).call()
+        this.setState({
+          products: [...this.state.products, product]
+        })
+      }
       this.setState({ loading: false})
     } else {
       window.alert('Marketplace contract not deployed to detected network.')
@@ -57,11 +64,20 @@ class App extends Component {
     }
 
     this.createProduct = this.createProduct.bind(this)
+    this.purchaseProduct = this.purchaseProduct.bind(this)
   }
 
   createProduct(name, price) {
     this.setState({ loading: true })
     this.state.marketplace.methods.createProduct(name, price).send({ from: this.state.account })
+    .once('receipt', (receipt) => {
+      this.setState({ loading: false })
+    })
+  }
+
+  purchaseProduct(id, price) {
+    this.setState({ loading: true })
+    this.state.marketplace.methods.purchaseProduct(id, price).send({ from: this.state.account, value: price })
     .once('receipt', (receipt) => {
       this.setState({ loading: false })
     })
@@ -76,7 +92,10 @@ class App extends Component {
             <main role="main" className="col-lg-12 d-flex">
               { this.state.loading
                 ? <div id="loader" className="text-center"><p className="text-center">Loading...</p></div>
-                : <Main createProduct={this.createProduct} />
+                : <Main
+                products={this.state.products}
+                createProduct={this.createProduct}
+                purchaseProduct={this.purchaseProduct} />
               }
             </main>
           </div>
